552005edd895af0103fcee86402314a98ab5829a
Arijit Das <59313848+Arijit-Das-07@users.noreply.github.com>
2021-03-04 22:40:31 +0530

    background structure done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./styles.css";
-
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div className="App container">
+      <div className="main w-75 p-4 bg-dark">
+        <h6> Cool Man</h6>
+        <button className="btn btn-dark">
+          <h2> Lets Go!!</h2>
+        </button>
+      </div>
     </div>
   );
 }
